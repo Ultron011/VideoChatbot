8ff5ef8d734fd6c7fab1848e6a5e2b644f1d1e64
Ultron011 <ultroninfinity01@gmail.com>
2026-05-25 14:06:53 +0530

    chore: delete dead OpenAI/HeyGen PCM bridge, drop node-fetch dep

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
 import livekitTokenHandler from './api/livekit-token.js';
 
 dotenv.config();
